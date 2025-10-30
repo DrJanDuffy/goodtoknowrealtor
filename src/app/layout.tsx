d0b8c336a7b083c2cf4e8e97f9b795c7217411c8
genekellyboyle <DrDuffy@bhhsnv.com>
2025-10-30 08:52:51 -0700

    Readability: content-flow max width, improved typography/contrast, applied sitewide

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -224,7 +224,7 @@ export default function RootLayout({
             Skip to navigation
           </a>
         <Navigation />
-        <main id="main-content" className='flex-1'>{children}</main>
+        <main id="main-content" className='flex-1 content-flow'>{children}</main>
         <Footer />
         <MobileStickyCTA />
         <WebVitalsMonitor />
